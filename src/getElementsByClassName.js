6e3b903fcf3f2129aaa89fbe186d8501f82a0324
Satoko Lom <stogamori@gmail.com>
2014-08-08 09:33:43 -0700

    ClassName.js fixed a little bug

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -14,29 +14,29 @@ var getElementsByClassName = function(className){
 		output.push(body);
 	};
 
-	//a function to check the array of nodes if it has the targeted class
-	var checkClass = function(arr){
-		var list = arr.classList;
+	//a function to check the element if it has the targeted class
+	var checkClass = function(ele){
+		var list = ele.classList;
 		for(var z=0; z<list.length; z++){
 			if(list[z] === className){
-				output.push(arr[i]);
+				output.push(ele);
 			};
+		};
 	};
 
-	//check direct children of body
+	// //check direct children of body
 	var directs = body_.childNodes;
-	for(var i=1; i<directs.length; i+2){
-		checkClass(directs);
-		};
+	for(var i=1; i < directs.length; i=i+2){
+		checkClass(directs[i]);
 	};
 
 	//check smaller than grandchildren
 	var check = function(stage){
 		if(stage.length != 0){
-			for(var a=0; a<stage.length; a++){
-				var nodelist = stage[a].childNodes;
+			for(var j=0; j<stage.length; j++){
+				var nodelist = stage[j].childNodes;
 				for(var k=0; k<nodelist.length; k++){
-					checkClass(nodelist);
+					checkClass(nodelist[k]);
 				};
 			};	
 		};
