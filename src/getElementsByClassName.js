17d9f6ea882f5f68e9175d8cbf44519668f898e9
Satoko Lom <stogamori@gmail.com>
2014-08-08 08:36:06 -0700

    add code to check body and direct children

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,4 +6,35 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
   // your code here
+	var output = [];
+	var body_ = document.body;
+
+	// check body itself first
+	if(body_.classList === [className]){
+		output.push(body);
+	};
+
+	//check direct children of body
+	var directs = body_.childNodes;
+	for(var i=1; i<directs.length; i+2){
+		var list = directs[i].classList;
+		for(var j=0; j<list.length; j++){
+			if(list[j] === className){
+				output.push(directs[i]);
+			};
+		};
+	};
+
+	//check smaller than grandchildren
+	// var classCheck = function(inside){
+	// 	for(var i=0; i<inside.childNodes.length; i++){
+	// 		if(body.classList[i] === className){
+	// 			return output.push(body);
+	// 		};
+	// 	};
+	// 	return output;
+	// };
+
+	// body.classCheck(body);
+
 };
